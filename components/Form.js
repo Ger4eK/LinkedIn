25c714fca33f11d8e50dd740a748c3741092bcb1
Ger4eK <ger4ek228@gmail.com>
2022-08-18 13:48:59 +0300

    POST request from front-end

--- components/Form.js
+++ components/Form.js
@@ -1,12 +1,36 @@
 import React, { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/modalAtoms';
 
 const Form = () => {
+  const { data: session } = useSession();
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
   const [input, setInput] = useState('');
   const [photoUrl, setPhotoUrl] = useState('');
 
-  const uploadPost = async (event) => {
-    event.preventDefault();
+  const uploadPost = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        input: input,
+        photoUrl: photoUrl,
+        username: session.user.name,
+        email: session.user.email,
+        userImg: session.user.image,
+        createdAt: new Date().toString(),
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const responseData = await response.json();
+
+    setModalOpen(false);
   };
+
   return (
     <form className='flex flex-col relative space-y-2 text-black/80 dark:text-white/75'>
       <textarea
